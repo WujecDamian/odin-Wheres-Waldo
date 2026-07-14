58576a99fcb8c2418565b6c9e988644c872fd516
Damian Wujec <wujecdamian12@gmail.com>
2026-07-14 14:16:30 +0200

    feat: implement getting relative percentage value where mouse is / click happened
    + get where cursor on img is (px)
    + get width and height of image
           + update on resize
    + calculate percentage value of where user clicked (x & y)

--- Frontend/src/components/game/game-board/GameBoard.jsx
+++ Frontend/src/components/game/game-board/GameBoard.jsx
@@ -1,11 +1,71 @@
+import { useState, useRef, useEffect } from "react";
 import styles from "./GameBoard.module.css";
 
 const GameBoard = (props) => {
+  const [clientX, setClientX] = useState(0);
+  const [clientY, setClientY] = useState(0);
+  const refElement = useRef(null);
+  const [refX, setRefX] = useState(0);
+  const [refY, setRefY] = useState(0);
+  const [imgX, setImgX] = useState(0);
+  const [imgY, setImgY] = useState(0);
+  const [percentageX, setPercentageX] = useState(0);
+  const [percentageY, setPercentageY] = useState(0);
+
+  useEffect(() => {
+    const width = refElement.current.offsetWidth;
+    const height = refElement.current.offsetHeight;
+
+    setImgX(width);
+    setImgY(height);
+  }, []);
+
+  useEffect(() => {
+    setRefX(imgX);
+    setRefY(imgY);
+  }, [imgX, imgY]);
+  //get actual img width and height (updates when page is resized)
+  const handleResize = (e) => {
+    window.addEventListener("resize", () => {
+      setImgX(refElement.current.offsetWidth);
+      setImgY(refElement.current.offsetHeight);
+    });
+  };
+  handleResize();
+
+  const mouseMoveHandler = (e) => {
+    // .getBoundingClientRect() helps track coordinates relative to the board itself
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    setClientX(Math.round(x));
+    setClientY(Math.round(y));
+
+    const clientRefX = (clientX / refX) * 100;
+    const clientRefY = (clientY / refY) * 100;
+    setPercentageX(clientRefX);
+    setPercentageY(clientRefY);
+  };
+
   return (
     <>
       <section className={styles.game__board}>
-        <img src={props.imgSrc} alt="" className={styles.board__image} />
+        <img
+          src={props.imgSrc}
+          alt=""
+          className={styles.board__image}
+          draggable="false"
+          onMouseMove={mouseMoveHandler}
+          ref={refElement}
+        />
       </section>
+      <p className="debug">
+        Image X: {clientX} | Image Y: {clientY}
+        <br></br>
+        Image Width: {refX} | Image Height: {refY}
+        <br></br>[ Relative % ] X: {percentageX} | Y: {percentageY}
+      </p>
     </>
   );
 };
