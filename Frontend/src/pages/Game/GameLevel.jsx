7fcb88cb9733c7f2fdd15ca2be222a0d51a12ea9
Damian Wujec <wujecdamian12@gmail.com>
2026-07-15 01:32:54 +0200

    feat: create mockup gameObj, characterList and pass ass props to gameBoard
    ! in future will be fetched from backend

--- Frontend/src/pages/Game/GameLevel.jsx
+++ Frontend/src/pages/Game/GameLevel.jsx
@@ -3,12 +3,40 @@ import GameBoard from "../../components/game/game-board/GameBoard";
 
 const GameLevel = (props) => {
   /* From params get level info from db and pass it to gameboard as props */
+  const gameObj = {
+    img_src:
+      "https://vuss.io/wp-content/uploads/2025/01/Wheres-Waldo-Beach-Super-High-Resolution-scaled.jpg",
+  };
+  const characterList = [
+    {
+      id: 1,
+      level_id: 2,
+      character_name: "Waldo",
+      img_url:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVgH6Z84jR4HnWfFdWLGJWRFUeKtX1pHMDVU_12GUrbh-ceiHznlcp6Ysi&s=10",
+      x1: 60.5,
+      x2: 63.0,
+      y1: 36.5,
+      y2: 41.5,
+    },
+    {
+      id: 2,
+      level_id: 2,
+      character_name: "Wilma",
+      img_url:
+        "https://static.wikia.nocookie.net/waldo/images/f/fe/Character_-_Wenda.png/revision/latest/smart/width/386/height/259?cb=20251124164901",
+      x1: 76.5,
+      x2: 77.8,
+      y1: 39.5,
+      y2: 42.5,
+    },
+  ];
   return (
     <>
       <p className={styles.small__screen__notice}>
         *Swipe left or right to see the full image.
       </p>
-      <GameBoard imgSrc="https://vuss.io/wp-content/uploads/2025/01/Wheres-Waldo-Beach-Super-High-Resolution-scaled.jpg"></GameBoard>
+      <GameBoard gameObj={gameObj} characterList={characterList}></GameBoard>
     </>
   );
 };
