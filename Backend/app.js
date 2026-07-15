d0bb82e58a60175cd0b4b5fcaadab24b3a71347d
Damian Wujec <wujecdamian12@gmail.com>
2026-07-16 00:12:14 +0200

    chore: enable cors in express

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,8 @@
 import express from "express";
+import cors from "cors";
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 //routers
 import levelRouter from "./Routers/levelsRouter.js";
