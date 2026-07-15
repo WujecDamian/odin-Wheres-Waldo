174642567aaa6785ac9105397b7f33d07315ab56
Damian Wujec <wujecdamian12@gmail.com>
2026-07-16 00:12:40 +0200

    feat: add level fetching from api & pass level in prop to LevelBox component

--- Frontend/src/components/game/levelbox/LevelBox.jsx
+++ Frontend/src/components/game/levelbox/LevelBox.jsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
 import styles from "./LevelBox.module.css";
 
-const LevelBox = (props) => {
+const LevelBox = ({ level }) => {
   return (
     <section className={styles.level__box}>
-      <Link to="level/1">
+      <Link to={`level/${level.id}`}>
         <img
-          src={props.imgSrc}
+          src={level.img_url}
           alt="Where's Waldo level"
           className={styles.level__box__img}
         />
-        <h2 className={styles.level__box__title}>Beach</h2>
-        <h3 className={styles.level__box__difficulty}>Easy</h3>
+        <h2 className={styles.level__box__title}>{level.name}</h2>
+        <h3 className={styles.level__box__difficulty}>{level.difficulty}</h3>
       </Link>
     </section>
   );
--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -1,17 +1,37 @@
+import { useEffect, useState } from "react";
 import LevelBox from "../../components/game/levelbox/LevelBox";
 import styles from "./Home.module.css";
 
 function Home() {
+  const [levels, setLevels] = useState([]);
+
+  useEffect(() => {
+    const fetchLevels = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/getAllLevels");
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const result = await response.json();
+
+        console.log(result);
+
+        setLevels(result.allLevels);
+      } catch (err) {
+        console.error("Fetch failed", err);
+      }
+    };
+    fetchLevels();
+  }, []);
+  console.log(levels);
   return (
     <>
-      {/* Here you actually get all levels from db & use map function to show <LevelBox> for every level*/}
       <section className={styles.levels}>
         {/* here's just hard coded one */}
-        <LevelBox imgSrc="https://vuss.io/wp-content/uploads/2025/01/Wheres-Waldo-Beach-Super-High-Resolution-scaled.jpg"></LevelBox>
-        <LevelBox imgSrc="https://vuss.io/wp-content/uploads/2025/01/Wheres-Waldo-Beach-Super-High-Resolution-scaled.jpg"></LevelBox>
-        <LevelBox imgSrc="https://vuss.io/wp-content/uploads/2025/01/Wheres-Waldo-Beach-Super-High-Resolution-scaled.jpg"></LevelBox>
-        <LevelBox imgSrc="https://vuss.io/wp-content/uploads/2025/01/Wheres-Waldo-Beach-Super-High-Resolution-scaled.jpg"></LevelBox>
-        <LevelBox imgSrc="https://vuss.io/wp-content/uploads/2025/01/Wheres-Waldo-Beach-Super-High-Resolution-scaled.jpg"></LevelBox>
+        {levels.map((level) => (
+          <LevelBox level={level}></LevelBox>
+        ))}
       </section>
     </>
   );
